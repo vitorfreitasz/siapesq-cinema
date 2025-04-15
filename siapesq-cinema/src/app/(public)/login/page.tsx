6f48be8c63c00c6cce6005586288fc1a0984de01
Vitor Freitas <vitordeavilafreitas@gmail.com>
2025-04-15 03:21:14 -0300

    fix: adicionando funções handle para registro, login, e redirecionamento e armazenamento de token nos cookies, junto com algumas correções na responsividade

--- siapesq-cinema/src/app/(public)/login/page.tsx
+++ siapesq-cinema/src/app/(public)/login/page.tsx
@@ -1,23 +1,42 @@
 import FormLogin from "@/components/form-login";
+import { IUsers } from "@/interfaces/user.interface";
+import LoginRequest from "@/requests/login.request";
+import { cookies } from "next/headers";
 import Image from "next/image";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 import React from "react";
 
 export default function Login() {
+  const handleLogin = async (data: IUsers) => {
+    "use server";
+    const response = await LoginRequest(data?.nome, data?.senha);
+    return response;
+  };
+  const handleRedirect = async (token: string) => {
+    "use server";
+    const cookieStorage = await cookies();
+    cookieStorage?.set("token", token);
+    redirect("/home");
+  };
+
   return (
-    <main className="flex flex-col items-center justify-center w-screen h-screen px-4 bg-gradient-to-b from-gray-900 via-gray-800 to-gray-900 ">
-      <div className="flex flex-col items-center justify-center bg-gray-950 w-full max-w-sm min-h-max p-6 rounded-3xl">
+    <main className="flex flex-col items-center justify-center w-full min-h-screen px-4 py-6 bg-gradient-to-b from-gray-900 via-gray-800 to-gray-900 overflow-y-auto">
+      <div className="flex flex-col items-center justify-center bg-gray-950 w-full sm:max-w-md md:max-w-sm p-6 sm:p-8 rounded-3xl shadow-lg">
         <Image
           src="/banner-sc.png"
           alt="Logo da empresa."
           width={0}
           height={0}
           sizes="100vw"
-          className="w-2/3 rounded-lg mb-20 mt-5"
+          className="w-3/4 sm:w-3/4 md:w-2/3 rounded-lg mb-10 mt-5"
         />
-        <FormLogin />
+        <FormLogin handleRedirect={handleRedirect} handleLogin={handleLogin} />
         <p className="text-white text-sm mt-4">
-          Não tem conta? <Link href={"/register"} className="underline">Cadastre-se</Link>
+          Não tem conta?{" "}
+          <Link href={"/register"} className="underline">
+            Cadastre-se
+          </Link>
         </p>
       </div>
     </main>
--- siapesq-cinema/src/app/(public)/register/page.tsx
+++ siapesq-cinema/src/app/(public)/register/page.tsx
@@ -1,23 +1,42 @@
 import FormRegister from "@/components/form-register";
+import { IUsers } from "@/interfaces/user.interface";
+import RegisterRequest from "@/requests/register.request";
+import { cookies } from "next/headers";
 import Image from "next/image";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 import React from "react";
 
 export default function Register() {
+  const handleRegister = async (data: IUsers) => {
+    "use server";
+    const response = await RegisterRequest(data?.nome, data?.senha);
+    return response;
+  };
+
+  const handleRedirect = async (token: string) => {
+    "use server";
+    const cookieStorage = await cookies();
+    cookieStorage?.set("token", token);
+    redirect("/home");
+  };
   return (
-    <main className="flex flex-col items-center justify-center w-screen h-screen px-4 bg-gradient-to-b from-gray-900 via-gray-800 to-gray-900 text-white">
-      <div className="flex flex-col items-center justify-center bg-gray-950 w-full max-w-sm min-h-max p-6 rounded-3xl">
+    <main className="flex flex-col items-center justify-center w-full min-h-screen px-4 py-6 bg-gradient-to-b from-gray-900 via-gray-800 to-gray-900 overflow-y-auto">
+      <div className="flex flex-col items-center justify-center bg-gray-950 w-full sm:max-w-md md:max-w-sm p-6 sm:p-8 rounded-3xl shadow-lg">
         <Image
           src="/banner-sc.png"
           alt="Logo da empresa."
           width={0}
           height={0}
           sizes="100vw"
-          className="w-2/3 rounded-lg mb-20 mt-5"
+          className="w-3/4 sm:w-3/4 md:w-2/3 rounded-lg mb-10 mt-5"
         />
-        <FormRegister />
+        <FormRegister
+          handleRedirect={handleRedirect}
+          handleRegister={handleRegister}
+        />
         <p className="text-white text-sm mt-4">
-          Já tem conta?
+          Já tem conta?{" "}
           <Link href={"/login"} className="underline">
             Entrar!
           </Link>
