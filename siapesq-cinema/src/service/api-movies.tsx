46d6e5c6d6d7cbf7c7045d013061e7f9208c781d
Vitor Freitas <vitordeavilafreitas@gmail.com>
2025-04-15 03:18:19 -0300

    feat: criando instancias axios que conectarão com as duas apis

---
+++ siapesq-cinema/src/service/api-movies.tsx
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const ApiMovies = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
+  params: {
+    language: "pt-BR",
+  },
+  headers: {
+    Authorization: process.env.TMDB_API_KEY,
+  },
+});
+
+export default ApiMovies;
