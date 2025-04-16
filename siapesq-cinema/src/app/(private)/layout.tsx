816c585f068f174214005ebe4d1f94e228fee627
Vitor Freitas <vitordeavilafreitas@gmail.com>
2025-04-16 07:37:51 -0300

    fix: transformando navbar em use client, para estilizar com base no pathname

--- siapesq-cinema/src/app/(private)/layout.tsx
+++ siapesq-cinema/src/app/(private)/layout.tsx
@@ -1,13 +1,25 @@
 import NavBar from "@/components/navbar";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 export default function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const handleLogout = async () => {
+    "use server";
+    const cookieStorage = await cookies();
+    cookieStorage.delete("token");
+    redirect("/login");
+  };
+  const handleRedirect = async (pathname: string) => {
+    "use server";
+    redirect(pathname);
+  };
   return (
     <main className="flex flex-col items-center w-full min-h-screen bg-gradient-to-b from-gray-900 via-gray-800 to-gray-900 overflow-y-auto">
-      <NavBar />
+      <NavBar handleLogout={handleLogout} handleRedirect={handleRedirect} />
       <div className="pt-20">{children}</div>
     </main>
   );
--- siapesq-cinema/src/components/navbar/index.tsx
+++ siapesq-cinema/src/components/navbar/index.tsx
@@ -1,16 +1,16 @@
-import { cookies } from "next/headers";
+"use client";
 import Image from "next/image";
-import Link from "next/link";
-import { redirect } from "next/navigation";
+import { usePathname } from "next/navigation";
 import React from "react";
 
-export default function NavBar() {
-  const handleLogout = async () => {
-    "use server";
-    const cookieStorage = await cookies();
-    cookieStorage.delete("token");
-    redirect("/login");
-  };
+export default function NavBar({
+  handleLogout,
+  handleRedirect,
+}: {
+  handleLogout: () => Promise<never>;
+  handleRedirect: (pathname: string) => Promise<never>;
+}) {
+  const pathname = usePathname();
   return (
     <nav className="fixed top-0 left-0 z-50 w-full h-16 flex items-center justify-evenly bg-gray-800 shadow-xl">
       <Image
@@ -21,32 +21,42 @@ export default function NavBar() {
         sizes="100vw"
         className="w-32"
       />
-      <Link
-        href="/home"
-        className="text-white hover:text-blue-600 transition-colors duration-200"
+      <button
+        onClick={() => handleRedirect("/home")}
+        className={`transition-colors cursor-pointer duration-200 ${
+          pathname === "/home"
+            ? "text-white font-bold underline decoration-blue-500 underline-offset-4"
+            : "text-white hover:text-blue-600"
+        }`}
       >
         Início
-      </Link>
-      <Link
-        href="/top-hated"
-        className="text-white hover:text-blue-600 transition-colors duration-200"
+      </button>
+      <button
+        onClick={() => handleRedirect("/top-hated")}
+        className={`transition-colors cursor-pointer duration-200 ${
+          pathname === "/top-hated"
+            ? "text-white font-bold underline decoration-blue-500 underline-offset-4"
+            : "text-white hover:text-blue-600"
+        }`}
       >
         Mais amados
-      </Link>
-      <Link
-        href="/now-playing"
-        className="text-white hover:text-blue-600 transition-colors duration-200"
+      </button>
+      <button
+        onClick={() => handleRedirect("/now-playing")}
+        className={`transition-colors cursor-pointer duration-200 ${
+          pathname === "/now-playing"
+            ? "text-white font-bold underline decoration-blue-500 underline-offset-4"
+            : "text-white hover:text-blue-600"
+        }`}
       >
         Lançamentos
-      </Link>
-      <form action={handleLogout}>
-        <button
-          type="submit"
-          className="text-white hover:text-blue-600 cursor-pointer transition-colors duration-200"
-        >
-          Sair
-        </button>
-      </form>
+      </button>
+      <button
+        onClick={() => handleLogout()}
+        className="text-white hover:text-red-800 transition-colors cursor-pointer duration-200"
+      >
+        Sair
+      </button>
     </nav>
   );
 }
