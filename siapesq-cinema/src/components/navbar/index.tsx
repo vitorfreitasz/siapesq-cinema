3a7a6ec57acec27eadac2e10e020b5d24db4c27d
Vitor Freitas <vitordeavilafreitas@gmail.com>
2025-04-16 08:00:05 -0300

    feat: refatorando navbar para ter menu com dropdown nos dispositivos moveis

--- siapesq-cinema/src/components/navbar/index.tsx
+++ siapesq-cinema/src/components/navbar/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
-import React from "react";
+import React, { useState } from "react";
 
 export default function NavBar({
   handleLogout,
@@ -11,6 +11,33 @@ export default function NavBar({
   handleRedirect: (pathname: string) => Promise<never>;
 }) {
   const pathname = usePathname();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleToggleMenu = () => setIsMenuOpen((prev) => !prev);
+
+  const menuItems = [
+    {
+      label: "Início",
+      path: "/home",
+      onClick: () => handleRedirect("/home"),
+    },
+    {
+      label: "Mais amados",
+      path: "/top-hated",
+      onClick: () => handleRedirect("/top-hated"),
+    },
+    {
+      label: "Lançamentos",
+      path: "/now-playing",
+      onClick: () => handleRedirect("/now-playing"),
+    },
+    {
+      label: "Sair",
+      path: "/logout", // só pra manter consistência, mesmo que não use
+      onClick: () => handleLogout(),
+    },
+  ];
+
   return (
     <nav className="fixed top-0 left-0 z-50 w-full h-16 flex items-center justify-evenly bg-gray-800 shadow-xl">
       <Image
@@ -21,42 +48,54 @@ export default function NavBar({
         sizes="100vw"
         className="w-32"
       />
-      <button
-        onClick={() => handleRedirect("/home")}
-        className={`transition-colors cursor-pointer duration-200 ${
-          pathname === "/home"
-            ? "text-white font-bold underline decoration-blue-500 underline-offset-4"
-            : "text-white hover:text-blue-600"
-        }`}
-      >
-        Início
-      </button>
-      <button
-        onClick={() => handleRedirect("/top-hated")}
-        className={`transition-colors cursor-pointer duration-200 ${
-          pathname === "/top-hated"
-            ? "text-white font-bold underline decoration-blue-500 underline-offset-4"
-            : "text-white hover:text-blue-600"
-        }`}
-      >
-        Mais amados
-      </button>
-      <button
-        onClick={() => handleRedirect("/now-playing")}
-        className={`transition-colors cursor-pointer duration-200 ${
-          pathname === "/now-playing"
-            ? "text-white font-bold underline decoration-blue-500 underline-offset-4"
-            : "text-white hover:text-blue-600"
-        }`}
-      >
-        Lançamentos
-      </button>
-      <button
-        onClick={() => handleLogout()}
-        className="text-white hover:text-red-800 transition-colors cursor-pointer duration-200"
-      >
-        Sair
-      </button>
+      {/* Menu Desktop */}
+      <div className="hidden sm:flex w-2/3 justify-around gap-4 items-center">
+        {menuItems.map((item) => (
+          <button
+            key={item.label}
+            onClick={item.onClick}
+            className={`transition-color cursor-pointer duration-200 ${
+              pathname === item.path
+                ? "text-white font-bold underline decoration-blue-500 underline-offset-4"
+                : item.label === "Sair"
+                ? "text-white hover:text-red-800"
+                : "text-white hover:text-blue-600"
+            }`}
+          >
+            {item.label}
+          </button>
+        ))}
+      </div>
+
+      {/* Botão Mobile */}
+      <div className="sm:hidden">
+        <button
+          onClick={handleToggleMenu}
+          className="text-white text-2xl cursor-pointer focus:outline-none"
+        >
+          ☰
+        </button>
+      </div>
+      {/* Menu Mobile */}
+      {isMenuOpen && (
+        <div className="absolute top-16 left-0 w-full bg-gray-900 flex flex-col gap-4 items-center py-4 sm:hidden z-40">
+          {menuItems.map((item) => (
+            <button
+              key={item.label}
+              onClick={item.onClick}
+              className={`transition-colors cursor-pointer duration-200 ${
+                pathname === item.path
+                  ? "text-white font-bold underline decoration-blue-500 underline-offset-4"
+                  : item.label === "Sair"
+                  ? "text-white hover:text-red-800"
+                  : "text-white hover:text-blue-600"
+              }`}
+            >
+              {item.label}
+            </button>
+          ))}
+        </div>
+      )}
     </nav>
   );
 }
