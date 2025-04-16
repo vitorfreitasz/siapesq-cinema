a7374af2b8fde8dd0eca24388eb28e589f13d170
Vitor Freitas <vitordeavilafreitas@gmail.com>
2025-04-16 01:03:21 -0300

    feat: criando telas em alta e lançamento

---
+++ siapesq-cinema/src/app/(private)/em-alta/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function EmAlta() {
+  return (
+    <div className="w-full custom-scrollbar max-w-[100vw] px-4 sm:px-6 md:px-10 py-8 space-y-12 md:max-w-7xl mx-auto">
+      EmAlta
+    </div>
+  );
+}
---
+++ siapesq-cinema/src/app/(private)/lancamentos/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Lancamentos() {
+  return (
+    <div className="w-full custom-scrollbar max-w-[100vw] px-4 sm:px-6 md:px-10 py-8 space-y-12 md:max-w-7xl mx-auto">
+      Lancamentos
+    </div>
+  );
+}
